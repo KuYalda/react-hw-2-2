88f65ba751a925b9b228a3dd067f9f285c6d3d42
danil <reloaderlev@gmail.com>
2019-11-25 15:32:34 +0200

    Updates

---
+++ precache-manifest.769c45d645a0cadcbf92fa1ca9e58b99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a5cdcc4724fadb88bf1c45c87a50e49",
+    "url": "/react-hw-2-2/index.html"
+  },
+  {
+    "revision": "d3d5696622cfeaabd79d",
+    "url": "/react-hw-2-2/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "870db7cda0fdcf1ed92c",
+    "url": "/react-hw-2-2/static/css/main.c88259d8.chunk.css"
+  },
+  {
+    "revision": "d3d5696622cfeaabd79d",
+    "url": "/react-hw-2-2/static/js/2.65402ed5.chunk.js"
+  },
+  {
+    "revision": "870db7cda0fdcf1ed92c",
+    "url": "/react-hw-2-2/static/js/main.ecf6e0be.chunk.js"
+  },
+  {
+    "revision": "5e2410de30bcca53cb07",
+    "url": "/react-hw-2-2/static/js/runtime-main.ffd29362.js"
+  }
+]);
